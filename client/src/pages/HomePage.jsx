464ed6e53293680219b04ec80343c7f6872b8ce0
Moth <kampanee.moth@gmail.com>
2024-06-12 15:18:16 +0700

    feat: create homepage and function search with axios get trips data

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import SearchTravel from "../components/SearchTravel";
+
+function HomePage() {
+  return (
+    <section>
+      <SearchTravel />
+    </section>
+  );
+}
+
+export default HomePage;
